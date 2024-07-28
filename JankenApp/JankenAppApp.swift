37d0c19a5e7fac89c36a9a4050176b290ea77dcb
Yasunari Osakabe <yasunari.osakabe@init-inc.com>
2024-07-28 16:00:15 +0900

    Initial Commit

---
+++ JankenApp/JankenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JankenAppApp.swift
+//  JankenApp
+//
+//  Created by 小坂部泰成 on 2024/07/28.
+//
+
+import SwiftUI
+
+@main
+struct JankenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
